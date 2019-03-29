ac26db106253e898b5e2f661a04c3e0f744a2762
RassuK <RassuK@users.noreply.github.com>
2019-03-29 22:45:20 +0200

    added calculation capability

--- lib/main.dart
+++ lib/main.dart
@@ -25,18 +25,74 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  String output = "0";
+
+  String _output = "0";
+  double num1 = 0.0;
+  double num2 = 0.0;
+  String operand = "";
+
+  buttonPressed(String buttonText) {
+    if (buttonText == "CLEAR") {
+      _output = "0";
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else if (buttonText == "+" ||
+        buttonText == "-" ||
+        buttonText == "/" ||
+        buttonText == "X") {
+      num1 = double.parse(output);
+
+      operand = buttonText;
+
+      _output = "0";
+    } else if (buttonText == ".") {
+      if (_output.contains(".")) {
+        print("Already conatains a decimals");
+        return;
+      } else {
+        _output = _output + buttonText;
+      }
+    } else if (buttonText == "=") {
+      num2 = double.parse(output);
+
+      if (operand == "+") {
+        _output = (num1 + num2).toString();
+      }
+      if (operand == "-") {
+        _output = (num1 - num2).toString();
+      }
+      if (operand == "X") {
+        _output = (num1 * num2).toString();
+      }
+      if (operand == "/") {
+        _output = (num1 / num2).toString();
+      }
+
+      num1 = 0.0;
+      num2 = 0.0;
+      operand = "";
+    } else {
+      _output = _output + buttonText;
+    }
+
+    print(_output);
+
+    setState(() {
+      output = double.parse(_output).toStringAsFixed(2);
+    });
+  }
 
   Widget buildButton(String buttonText) {
     return new Expanded(
       child: new OutlineButton(
         padding: new EdgeInsets.all(24.0),
-        child: new Text(buttonText,
-        style: TextStyle(
-          fontSize: 20.0,
-          fontWeight: FontWeight.bold
+        child: new Text(
+          buttonText,
+          style: TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
         ),
-        ),
-        onPressed: () => {},
+        onPressed: () => buttonPressed(buttonText),
       ),
     );
   }
@@ -51,19 +107,17 @@ class _MyHomePageState extends State<MyHomePage> {
             child: new Column(
           children: <Widget>[
             new Container(
-              alignment: Alignment.centerRight,
-              padding: new EdgeInsets.symmetric(
-                vertical: 24.0,
-                horizontal: 12.0
-              ),
-              child: new Text("output", style: new TextStyle(
-                fontSize: 48.0,
-                fontWeight: FontWeight.bold,
-                
-              ))),
+                alignment: Alignment.centerRight,
+                padding:
+                    new EdgeInsets.symmetric(vertical: 24.0, horizontal: 12.0),
+                child: new Text(output,
+                    style: new TextStyle(
+                      fontSize: 48.0,
+                      fontWeight: FontWeight.bold,
+                    ))),
             new Expanded(
               child: new Divider(),
-),
+            ),
             new Column(children: [
               new Row(children: [
                 buildButton("7"),
